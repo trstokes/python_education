45dddcea5ffa0a0b406011333202b52869fb30f9
Tehran Stokes <tehranalmasi@Tehrans-MacBook-Pro.local>
2022-01-22 16:11:43 -0600

    Pateron practice

--- Introduction.py
+++ Introduction.py
@@ -1,3 +1,23 @@
 'This is a test'
 
 'This is a pull test'
+
+
+x = 50
+y = 500
+
+platinum = x * y
+print(platinum)
+
+
+x = 25
+y = 1500
+gold = x * y
+print(gold)
+
+x = 15
+y = 2500
+silver = x * y
+print(silver)
+
+print(platinum + gold + silver) # returns 100,000
