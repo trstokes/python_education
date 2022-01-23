4df8dbef48c783b883b96dc52fadaf270b9bab9e
Tehran Stokes <tehranalmasi@Tehrans-MacBook-Pro.local>
2022-01-23 14:02:02 -0600

    Python Syntax Module

---
+++ Python_Syntax.py
@@ -0,0 +1,26 @@
+# Lecture Example 1: 
+# lst = []
+
+# for i in range(1, 11):
+#     lst.append(i)
+# print(lst)
+
+# lst1 = [i for i in range(1, 11)]
+# print(lst1)
+
+# Lecture Example 2: Multiple Assignment
+# x = y = 1 
+# print(x) # prints 1
+
+# x, y = 1, 2 
+# print(x, y) # prints 1 2 
+
+# x, y, z = [1, 2, 3]
+# print(x, y, z) # 1 2 3 
+
+# Lecture Example 3: Docstrings 
+# def foo():
+#     """
+#     this is the foo function
+#     """
+#     pass 
