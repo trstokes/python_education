a9e1d7c884cdc42b3f22aa413b4a33a1f15bf337
Tehran Stokes <tehranalmasi@Tehrans-MacBook-Pro.local>
2022-01-23 12:54:39 -0600

    Scope Module

---
+++ Scope.py
@@ -0,0 +1,31 @@
+#  Question What does the following program print?
+#x = []
+
+#def foo(y):
+    #y.append(1)
+
+#foo(x)
+#print(x)
+
+# Question - What does the following program print? 
+
+# x = []
+
+# def foo(y):
+#     y.append(1)
+#     y = [2]
+
+# foo(x)
+# print(x)
+
+# Quesstion - What does the following program print? 
+# x = 'global'
+
+# def foo():
+#     global x
+#     print(x, end=",")
+#     x = 'local'
+#     print(x, end=",")
+
+# foo()
+# print(x, end="")
