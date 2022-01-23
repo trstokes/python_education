3ecc9be953841a6605cbaf0b54e5ab8180220231
Tehran Stokes <tehranalmasi@Tehrans-MacBook-Pro.local>
2022-01-23 13:15:06 -0600

    Math Module

---
+++ Math.py
@@ -0,0 +1,23 @@
+import random
+
+# random_number = random.randint(1, 10)
+# print(random_number)
+
+# lst = ['he', 'hi', 'ho', 'hello']
+# random_number = random.choice(lst)
+# print(random_number)
+
+## Question - What does the following code output?
+# number = -2.765261
+
+# print(round(number, 2))
+
+
+# numbers = [-3, 4, 2, 1, -2]
+    
+# print(sum(numbers)) # -2.77
+
+## Question - What does the following code output?
+# string = "hello world"
+
+# print(max(string)) # w
