5eb0aba25c9d12242f7018a2f17716cade253b47
Tehran Stokes <tehranalmasi@Tehrans-MacBook-Pro.local>
2022-01-28 12:02:08 -0600

    Updated Numbers

--- Introduction.py
+++ Introduction.py
@@ -4,20 +4,20 @@
 
 
 x = 50
-y = 500
+y = 250
 
 platinum = x * y
 print(platinum)
 
 
-x = 25
-y = 1500
+x = 35
+y = 500
 gold = x * y
 print(gold)
 
-x = 15
-y = 2500
+x = 20
+y = 1000
 silver = x * y
 print(silver)
 
-print(platinum + gold + silver) # returns 100,000
+print(platinum + gold + silver) # returns 50,000
