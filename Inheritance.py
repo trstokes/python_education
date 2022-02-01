d5a258c1271547fe53bcc13b485405cb37b56b22
Tehran Stokes <tehranalmasi@Tehrans-MacBook-Pro.local>
2022-02-01 16:21:02 -0600

    Refresh

--- Inheritance.py
+++ Inheritance.py
@@ -52,18 +52,16 @@ class Mammal(Animal):
         super().__init__(age, weight, height)
         self.sex = sex 
 
+class Reptile(Animal):
+    def __init__(self, age, weight, height, legs):
+        super().__init__(age, weight, height)
+        self.legs = legs 
+
 class Monkey(Mammal):
     fingers = 5 
     def __init__(self, age, weight, height, sex, color):
         super().__init__(age, weight, height, sex)
         self.color = color
-
-
-class Reptile(Animal):
-    def __init__(self, age, weight, height, legs):
-        super().__init__(age, weight, height)
-        self.legs = legs 
-    
 
 class Lizard(Reptile):
     tail = True 
