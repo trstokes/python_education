4ad542d358201acc40ec272d460af65a3358d3a4
Tehran Stokes <tehranalmasi@Tehrans-MacBook-Pro.local>
2022-02-01 17:30:34 -0600

    Abstract Classes

---
+++ abstract_classes.py
@@ -0,0 +1,81 @@
+# Guesser Game # 
+
+import random
+
+class AbstractGame:
+    def start(self):
+        while True:
+            start = input('Would you like to play?')
+            if start.lower() == 'yes':
+                break
+
+        self.play()
+
+
+    
+    def end(self):
+        print('The game has ended.')
+        self.reset()
+    
+    def play(self):
+        raise NotImplementedError('You must provide an implementaion for play()')
+
+    def reset(self):
+        raise NotImplementedError('You must provide an implementation for reset()')
+
+
+class RandomGuesser(AbstractGame):
+    def __init__(self, rounds):
+        self.rounds = rounds 
+        self.round = 0
+
+    def reset(self):
+        self.round = 0 
+
+    def play(self):
+        while self.round < self.rounds:
+            self.round += 1
+
+            print(f"Welcome to round {self.round}. Let's begin!")
+            random_num = random.randint(0, 10)
+            while True:
+                guess = input('Enter a number between 1-10: ')
+                if int(guess) == random_num:
+                    print("You got it!")
+                    break 
+            
+        self.end()
+
+
+# game = RandomGuesser(2)
+# game.play()
+# game.play()
+
+## Abstract Animal Class ## 
+
+class AbstractAnimal: 
+
+    def sleep(self):
+        print('ZzzZzz')
+
+    def wake_up(self):
+        self.animal_sound()
+        print('I am awake!')
+
+    def animal_sound(self):
+        raise NotImplementedError('You must provide an Implementation for animal_sound()')
+
+class Lion(AbstractAnimal):
+
+    def animal_sound(self):
+        print('Roar!')
+
+l = Lion()
+
+l.animal_sound()
+        
+
+
+
+
+        
